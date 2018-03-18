d5115078e5b9e8b200753f197edd5c1eb52dd4ad
MasterOfTheTiger <ted.programmer@gmail.com>
2018-03-17 18:15:12 -0700

    Add Logo

--- config.js
+++ config.js
@@ -1,2 +1,11 @@
-var list = ["","#bg","#scr","#gh","#twt","#mail","#amo","#apod"];
-var urls = ["http://mtiger.tk","http://blog.mtiger.tk","https://scratch.mit.edu/users/masterofthetiger","https://github.com/masterofthetiger","https://twitter.com/masterothetiger","mailto:ted.programmer@gmail.com","https://addons.mozilla.org/en-US/firefox/user/MasterOfTheTiger/","https://addons.mozilla.org/en-US/firefox/addon/astronomy-picture/"];
+var list = ["","#bg","#scr","#gh","#twt","#mail","#amo","#apod","#logo"];
+var urls = [
+  "http://mtiger.tk",
+  "http://blog.mtiger.tk",
+  "https://scratch.mit.edu/users/masterofthetiger",
+  "https://github.com/masterofthetiger","https://twitter.com/masterothetiger",
+  "mailto:ted.programmer@gmail.com",
+  "https://addons.mozilla.org/en-US/firefox/user/MasterOfTheTiger/",
+  "https://addons.mozilla.org/en-US/firefox/addon/astronomy-picture/",
+  "http://cubeupload.com/im/MasterOfTheTiger/776logo.png"
+];
