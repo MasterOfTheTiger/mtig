c7e82e97c9954068adafc5e3ab04fc494057dded
MasterOfTheTiger <ted.programmer@gmail.com>
2018-03-17 17:22:32 -0700

    Add Twitter

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-var list = ["","#bg","#scr","gh"];
-var urls = ["http://mtiger.tk","http://blog.mtiger.tk","https://scratch.mit.edu/users/masterofthetiger","https://github.com/masterofthetiger"];
+var list = ["","#bg","#scr","gh","#twt];
+var urls = ["http://mtiger.tk","http://blog.mtiger.tk","https://scratch.mit.edu/users/masterofthetiger","https://github.com/masterofthetiger","https://twitter.com/masterofthetiger"];
