d5755dc2f47b3f6b2095489485ac0c69e25a30bf
MasterOfTheTiger <ted.programmer@gmail.com>
2018-03-17 17:31:48 -0700

    Fix syntax error

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-var list = ["","#bg","#scr","gh","#twt];
+var list = ["","#bg","#scr","gh","#twt"];
 var urls = ["http://mtiger.tk","http://blog.mtiger.tk","https://scratch.mit.edu/users/masterofthetiger","https://github.com/masterofthetiger","https://twitter.com/masterofthetiger"];
