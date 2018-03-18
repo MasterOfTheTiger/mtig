4079c9214bd3eefb86473f56f3362d09e0a219c4
MasterOfTheTiger <ted.programmer@gmail.com>
2018-03-17 18:07:46 -0700

    Fix Twitter link

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 var list = ["","#bg","#scr","#gh","#twt","#mail","#amo","#apod"];
-var urls = ["http://mtiger.tk","http://blog.mtiger.tk","https://scratch.mit.edu/users/masterofthetiger","https://github.com/masterofthetiger","https://twitter.com/masterofthetiger","mailto:ted.programmer@gmail.com","https://addons.mozilla.org/en-US/firefox/user/MasterOfTheTiger/","https://addons.mozilla.org/en-US/firefox/addon/astronomy-picture/"];
+var urls = ["http://mtiger.tk","http://blog.mtiger.tk","https://scratch.mit.edu/users/masterofthetiger","https://github.com/masterofthetiger","https://twitter.com/masterothetiger","mailto:ted.programmer@gmail.com","https://addons.mozilla.org/en-US/firefox/user/MasterOfTheTiger/","https://addons.mozilla.org/en-US/firefox/addon/astronomy-picture/"];
