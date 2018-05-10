f9a1ffb7d032ed7dd960623b10f4da34d57a489f
MasterOfTheTiger <ted.jameson@pm.me>
2018-05-10 10:50:27 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var list = ["","#bg","#scr","#gh","#twt","#mail","#amo","#apod","#logo","#diasp"];
+var list = ["","#bg","#scr","#gh","#twt","#mail","#amo","#apod","#logo","#diasp", "#fednh"];
 var urls = [
   "http://mtiger.tk",
   "http://blog.mtiger.tk",
@@ -8,5 +8,6 @@ var urls = [
   "https://addons.mozilla.org/en-US/firefox/user/MasterOfTheTiger/",
   "https://addons.mozilla.org/en-US/firefox/addon/astronomy-picture/",
   "http://cubeupload.com/im/MasterOfTheTiger/776logo.png",
-  "https://diasp.org/people/d91b07b00d2101360007047d7b62795e"
+  "https://diasp.org/people/d91b07b00d2101360007047d7b62795e",
+  "https://diasp.org/posts/cb44118036a6013698db047d7b62795e"
 ];
